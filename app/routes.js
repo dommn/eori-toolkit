ba7a2b468a04a999a34c8a4f1687241624b7663b
dommn <47447454+dommn@users.noreply.github.com>
2022-11-23 09:24:14 +0000

    Changes based on limited usability feedback and design crit

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,9 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+// Start folder specific routes
+
+// current sprint, remember to add older sprint when adding a new folder!
+router.use('/01', require('./views/01/_routes'));
+
 module.exports = router
