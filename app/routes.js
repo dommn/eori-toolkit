7ffa4b455d9e004a550ffdff50d4445cd88acde4
dommn <47447454+dommn@users.noreply.github.com>
2022-12-05 10:55:22 +0000

    Added v3

--- app/routes.js
+++ app/routes.js
@@ -8,5 +8,6 @@ const router = express.Router()
 // current sprint, remember to add older sprint when adding a new folder!
 router.use('/01', require('./views/01/_routes'));
 router.use('/02', require('./views/02/_routes'));
+router.use('/03', require('./views/03/_routes'));
 
 module.exports = router
