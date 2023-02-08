5c4a6e7ee0797e1b510fa2ee3fb2dadad6b91ecf
dommn <47447454+dommn@users.noreply.github.com>
2023-02-08 10:15:33 +0000

    Added 05 and UR changes

--- app/routes.js
+++ app/routes.js
@@ -11,5 +11,6 @@ router.use('/02', require('./views/02/_routes'));
 router.use('/03', require('./views/03/_routes'));
 router.use('/04', require('./views/04/_routes'));
 router.use('/ur', require('./views/ur/_routes'));
+router.use('/05', require('./views/05/_routes'));
 
 module.exports = router
---
+++ app/views/05/_routes.js
@@ -0,0 +1,84 @@
+const express = require('express')
+const router = express.Router()
+// Add your routes here - above the module.exports line
+
+router.post('/update-or-cancel', function (req, res) {
+
+  const updateEori = req.session.data['update-or-cancel']
+
+  if (updateEori === 'Update an EORI number') {
+    res.redirect('update-an-eori-number')
+  } else {
+    res.redirect('enter-an-eori-number')
+  }
+})
+
+router.post('/update-or-cancel-2', function (req, res) {
+
+  const updateEori = req.session.data['update-or-cancel']
+
+  if (updateEori === 'Update an EORI number') {
+    res.redirect('update-an-eori-number')
+  } else {
+    res.redirect('enter-an-eori-number-2')
+  }
+})
+
+router.post('/success-or-fail', function (req, res) {
+
+  let successFail = req.session.data['update-current-eori-number']
+
+  if (successFail === 'GB205672212000') {
+    res.redirect('current-enrolments')
+  }
+  else if (successFail === 'GB612599232321') {
+    res.redirect('current-enrolments-2')
+  }
+})
+
+router.post('/enter-eori', function (req, res) {
+
+  let successFail = req.session.data['current-eori-number']
+
+  if (successFail === 'GB205672212000') {
+    res.redirect('success-cancel')
+  }
+  else if (successFail === 'GB612599232321') {
+    res.redirect('services-couldnt-update')
+  }
+})
+
+router.post('/cancel-subscription', function (req, res) {
+
+  const cancelSubscription = req.session.data['cancel-subscription']
+
+  if (cancelSubscription === 'yes') {
+    res.redirect('success-cancel')
+  } else if (cancelSubscription === 'no') {
+    res.redirect('enter-an-eori-number')
+  }
+})
+
+router.post('/cancel-subscription-2', function (req, res) {
+
+  const cancelSubscription = req.session.data['cancel-subscription']
+
+  if (cancelSubscription === 'yes') {
+    res.redirect('services-couldnt-update')
+  } else if (cancelSubscription === 'no') {
+    res.redirect('enter-an-eori-number-2')
+  }
+})
+
+router.post('/replace-eori', function (req, res) {
+
+  const replaceEori = req.session.data['replace-eori']
+
+  if (replaceEori === 'yes') {
+    res.redirect('update-or-cancel-success-notification')
+  } else if (replaceEori === 'no') {
+    res.redirect('update-an-eori-number')
+  }
+})
+
+module.exports = router
