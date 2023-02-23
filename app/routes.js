93518d1631ccd4d50a19ee709aa6f5fb69044e34
dommn <47447454+dommn@users.noreply.github.com>
2023-02-23 11:31:50 +0000

    Adde updated UR flow

--- app/routes.js
+++ app/routes.js
@@ -12,6 +12,7 @@ router.use('/03', require('./views/03/_routes'));
 router.use('/04', require('./views/04/_routes'));
 router.use('/ur', require('./views/ur/_routes'));
 router.use('/ur-2', require('./views/ur-2/_routes'));
+router.use('/ur-3', require('./views/ur-3/_routes'));
 router.use('/05', require('./views/05/_routes'));
 
 module.exports = router
